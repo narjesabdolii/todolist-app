808860ddb2e3f90d726bd9ab83ad786bdd05f139
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-14 13:59:04 +0330

    instantiate homescreen in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:todolist/Screens/Home.dart';
 void main() {
-  runApp();
+  runApp(const GetMaterialApp(
+    home: HomeScreen(),
+  ));
 }
 
