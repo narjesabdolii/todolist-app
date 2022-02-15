06c611eb52673c6bf528ce0ed16a990d46dc3968
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-15 13:30:22 +0330

    add title to todo model

--- lib/models/ToDo.dart
+++ lib/models/ToDo.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ToDo{
+  String title;
   String text;
   bool done;
   ToDo({
   required this.text,
     required this.done,
-       });
+    required this.title,
+  });
 }
