9e972e5597044349c969dd7151cac80842c2d712
Ashkan Tofangdar <ashkan4472@gmail.com>
2022-02-17 14:52:45 +0330

    changed main to have stateLess Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todolist/Screens/Home.dart';
+
 void main() {
-  runApp( GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    darkTheme: ThemeData.dark(),
-    themeMode: ThemeMode.system,
-    home: const HomeScreen(),
-  ));
+  runApp(const TodoApp());
 }
 
+class TodoApp extends StatelessWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      theme: ThemeData(fontFamily: "Poppins-Regular"),
+      home: const HomeScreen(),
+    );
+  }
+}
